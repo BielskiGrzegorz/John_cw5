7d6df98a987ea6b6b7703e6a9bb755283fd518b7
BielskiGrzegorz <bielskigrzegorz@outlook.com>
2017-11-06 13:01:36 +0100

    Cw5
    lalala

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+#include "Probka.HPP"
+#include "Analizator.HPP"
+#include "Loader.HPP"
+#include "Sygnal.HPP"
+
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+    if(argc !=2)
+    {
+        return -1;
+    }
+
+
+    return 0;
+}
